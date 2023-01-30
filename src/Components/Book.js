31eeceb820899dc3850aff0d3c66c8732ae90be0
Juanpa8830 <jp3_4@hotmail.com>
2023-01-30 13:49:55 -0500

    Update reducer and component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/book';
 
 const BookItem = (props) => {
+  const dispatch = useDispatch();
   const {
     id, category, title, author,
   } = props;
+
+  const onClick = () => {
+    dispatch(removeBook(id));
+  };
 
   return (
 
@@ -12,7 +19,7 @@ const BookItem = (props) => {
       <h4>{category}</h4>
       <h2>{title}</h2>
       <h3>{author}</h3>
-      <button type="button" id={id}>Remove</button>
+      <button onClick={onClick} type="button">Remove</button>
 
     </li>
 
--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,29 +1,52 @@
-const bookReducer = (state = [], action) => {
+const initialState = [{
+  id: 0,
+  category: 'Action',
+  title: 'The Hunger Games',
+  author: 'Suzanne Collins',
+  chapters: 25,
+  currentChapter: 16,
+},
+{
+  id: 1,
+  category: 'Science Fiction',
+  title: 'Dune',
+  author: 'Frank Herbert',
+  chapters: 25,
+  currentChapter: 2,
+},
+{
+  id: 2,
+  category: 'Economy',
+  title: 'Capital in the Twenty-First century',
+  author: 'Suzanne Collins',
+  chapters: 30,
+  currentChapter: 13,
+}];
+
+const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_BOOK':
       return [
         ...state, action.payload,
       ];
     case 'REMOVE_BOOK':
-      return state.filter((book) => book.id !== action.payload.id);
+      return state.filter((book) => book.id !== action.payload);
     default:
       return state;
   }
 };
 
-export const AddBook = (id, category, title, author) => ({
+export const AddBook = (book) => ({
 
   type: 'ADD_BOOK',
-  payload: {
-    id, category, title, author,
-  },
+  payload: book,
 
 });
 
 export const removeBook = (id) => ({
 
   type: 'REMOVE_BOOK',
-  payload: { id },
+  payload: id,
 
 });
 
