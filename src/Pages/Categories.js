3a64ed7cdadddcea58ab1688ff31daf4572e3d6c
Juanpa8830 <jp3_4@hotmail.com>
2023-01-30 13:49:09 -0500

    Add functionality and dispatch chekhStatus action

--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -1,6 +1,25 @@
-const Categories = () => (
-  <button type="button" className="checkstbtn">CHECK STATUS</button>
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-);
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+
+  const dispatch = useDispatch();
+  const onClick = () => {
+    dispatch(checkStatus('under construction'));
+  };
+  return (
+    <div>
+      <h2>{status}</h2>
+      <button
+        type="button"
+        className="checkstbtn"
+        onClick={onClick}
+      >
+        CHECK STATUS
+      </button>
+    </div>
+  );
+};
 
 export default Categories;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -2,17 +2,17 @@ const categoriesReducer = (state = [], action) => {
   switch (action.type) {
     case 'CHECK_STATUS':
       return [
-        ...state, action.payload,
+        action.payload,
       ];
     default:
       return state;
   }
 };
 
-export const checkCategories = () => (
+export const checkStatus = (data) => (
   {
     type: 'CHECK_STATUS',
-    payload: 'Under construction',
+    payload: data,
   }
 );
 
