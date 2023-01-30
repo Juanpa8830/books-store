336927d9873d41a31a606bb3d8328c7cae3fb91d
Juanpa8830 <jp3_4@hotmail.com>
2023-01-30 13:47:50 -0500

    Connect store and dispatch add/remove actions

--- src/Components/BookInput.js
+++ src/Components/BookInput.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { AddBook } from '../redux/books/book';
 
-const BookInput = () => (
+const BookInput = () => {
+  const dispatch = useDispatch();
+  const [bookData, setbookData] = useState({ title: '', author: '' });
 
-  <div>
-    <form>
-      <input type="text" placeholder="Book title" required />
-      <input type="text" placeholder="Author" required />
-      <button type="submit" className="AdBtn">Add Book</button>
-    </form>
-  </div>
+  const onChange = (e) => {
+    setbookData({
+      ...bookData,
+      [e.target.name]: e.target.value,
+      // concatenar los valores de los inputs al estado de react inicial
+    });
+  };
 
-);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddBook({
+      ...bookData,
+      id: uuidv4(),
+      category: 'science-fiction',
+    }));
+    setbookData({ title: '', author: '' });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Book title"
+          required
+          value={bookData.title}
+          name="title"
+        />
+
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Author"
+          required
+          name="author"
+          value={bookData.author}
+        />
+        <button
+          type="submit"
+          className="AdBtn"
+        >
+          Add Book
+        </button>
+      </form>
+    </div>
+
+  );
+};
 
 export default BookInput;
